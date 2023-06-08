bf6ba62917287db4e5e97a9eeb2890299a72f98e
Better-Call-Frank <franklinemogaka@gmail.com>
2023-06-08 13:07:52 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Import variable a from variable_load_5 and print its value."""
-    import a from variable_load_5 
+    """Import a from variable_load_5 and print its value."""
 
     print(a)
