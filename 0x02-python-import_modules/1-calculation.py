d4b49d4c3de395fe6b4750f22afffa292f2669f1
Better-Call-Frank <franklinemogaka@gmail.com>
2023-06-08 02:47:07 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    """print sum, difference, multiplication and division of 10 and 5."""
+    from calculator_1 import add, multiply, divide, subtract
 
-result_add = add(a, b)
-result_subtract = subtract(a, b)
-result_multiply = multiply(a, b)
-result_divide = divide(a, b)
+    a = 10
+    b = 5
 
-print("The sum of", a, "and", b, "is:", result_add)
-print("The difference between", a, "and", b, "is:", result_subtract)
-print("The product of", a, "and", b, "is:", result_multiply)
-print("The division of", a, "and", b, "is:", result_divide)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} * {} = {}".format(a, b, multiply(a, b)))
+    print("{} / {} = {}".format(a, b, divide(a, b)))
+    print("{} - {} = {}".format(a, b, subtract(a, b)))
