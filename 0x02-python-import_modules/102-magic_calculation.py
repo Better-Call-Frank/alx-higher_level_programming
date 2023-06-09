10e1b5b2b270d1685aea6b06bf82a2d3f40c2a1b
Better-Call-Frank <franklinemogaka@gmail.com>
2023-06-08 19:58:19 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 
 
 def magic_calculation(a, b):
-    """match the bytecode provided by Holberton."""
+    """Match bytecode provided by Holberton."""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -13,4 +13,3 @@ def magic_calculation(a, b):
 
     else:
         return(sub(a, b))
-
