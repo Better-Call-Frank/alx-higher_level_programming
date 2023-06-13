63566c7ea5a60de6f04b6766917e42d6c3d4f20d
Better-Call-Frank <franklinemogaka@gmail.com>
2023-06-12 20:42:38 -0700

    7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 7-add_tuple.py
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """Add two tuples."""
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    sum_tuple = (a[0] + b[0], a[1] + b[1])
+    return sum_tuple
