109bbb1462dda7b6d98b5c3984905266f14392e6
Better-Call-Frank <franklinemogaka@gmail.com>
2023-06-12 20:23:13 -0700

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 5-no_c.py
+
+
+def no_c(my_string):
+    """Removes all characters c and C from a string."""
+    new_string = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            new_string += char
+    return new_string
