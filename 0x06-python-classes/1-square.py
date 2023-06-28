c04b23288dc0b13c87320b5b48a04927f3c93443
Better-Call-Frank <franklinemogaka@gmail.com>
2023-06-27 19:57:38 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize the size attribute."""
+
+        Args:
+            size (int): Size of square.
+        """
+        self.__size = size
