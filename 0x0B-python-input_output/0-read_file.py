38de98354d40acdf18891948f9457ed63c2115d5
Better-Call-Frank <franklinemogaka@gmail.com>
2023-07-12 06:31:31 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+"""Reads a text file and prints it to stdout."""
 
 def read_file(filename=""):
-    # Open the file in read mode with UTF-8 encoding
+    """Open the file in read mode with UTF-8 encoding."""
     with open(filename, "r", encoding="utf-8") as file:
-        # Read the entire content of the file as a string
+        """Read the entire content of the file as a string."""
         content = file.read()
 
-        # Print the content to stdout
+        """Print the content to stdout."""
         print(content)
